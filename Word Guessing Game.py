05a5d364d634287730ba3bd25830b79408ca7a27
Saira-dev7 <sairaameen243@gmail.com>
2025-12-20 15:48:16 +0500

    Add files via upload
    I designed a game in which player guesses characters of a random word.

---
+++ Word Guessing Game.py
@@ -0,0 +1,31 @@
+import random
+name=input("What is your name?")
+print("Good LucK", name)
+
+words=["rainbow","computer","science","programming","python","mathematics","player","condition","reverse","water","board","geeks"]
+word=random.choice(words)
+print("Guess the characters")
+guesses=""
+turns=12
+while turns>0:
+    failed=0
+    for char in word:
+        if char in guesses:
+            print(char,end=" ")
+        else:
+            print("_")
+            failed+=1
+            if failed==0:
+                print("You WIN!")
+                print("The word is: ", word)
+                break
+            print()
+            guess=input("Guess a character: ")
+            guesses+=guess
+
+            if guess not in word:
+                turns-=1
+                print("Wrong")
+                print("You have", +turns, "more guesses")
+                if turns==0:
+                    print("you Loose")
